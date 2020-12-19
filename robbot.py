c295db924f9bd270f4bf554b40f250f3d20bbf31
MonkfishFillet <MonkfishFillet@gmail.com>
2020-12-19 03:48:06 -0500

    Finished Bartender Embed Functionality

--- robbot.py
+++ robbot.py
@@ -1,5 +1,7 @@
 import os
 import discord
+from requests.api import get
+from util import Bartender
 from dotenv import load_dotenv
 
 SHEBANGS = '.!$'
@@ -30,6 +32,8 @@ async def on_message(message):
         pass
     if cmd == 'bg':
         await message.channel.send('hey')
+    if cmd == 'drink' or cmd == 'cocktail':
+        await get_cocktail(message)
     
     return
 
@@ -55,8 +59,37 @@ async def check_react_ohwow(message):
             if emoji.name == 'ohwow':
                 await message.add_reaction(emoji)
 
-# TODO Add Cocktail Function
-def get_cocktail(msg):
-    pass
+'''
+@author: Keeth S.
+@dependencies: util/Bartender.py
+@desc: Declares, fills and formats a discord embed object with drink info
+@retunrs: async message back to channgel
+# TODO Optimize Drink Object. Create Embed method?
+'''
+async def get_cocktail(msg):
+    try:
+        drink = Bartender.get_drink()
+        embed_var = discord.Embed(title='Buds Bartender', description='A drink for you, dear bud.', color=1146986)
+        embed_var.set_image(url=drink.img)
+        embed_var.add_field(name="Name", value=drink.name)
+        embed_var.add_field(name="Category", value=drink.category)
+        embed_var.add_field(name="\u200b", value='\u200b')
+        embed_var.add_field(name="\Alcoholic?", value=drink.alcoholic)
+        embed_var.add_field(name="Glass Type", value=drink.glass)
+        embed_var.add_field(name="\u200b", value='\u200b')
+        ingredient_string = ""
+        for string in drink.ingredients:
+            ingredient_string += string + '\n'
+        embed_var.add_field(name="Ingredients", value=ingredient_string, inline=False)
+        embed_var.add_field(name="Instructions", value=drink.instructions, inline=False)
+        embed_var.set_footer(text="Have ideas for additional functionality? Throw them in #robbot_discussion!")
+
+        await msg.channel.send(embed=embed_var)
+    
+    except Exception as ex:
+        print(ex)
+        await msg.channel.send('Ayo, your code is wack.')
+
+
 
 client.run(TOKEN)
