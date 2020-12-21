b2ed5a22ad12618830f9ca78ffae92cf372b7d61
Brady Glass <bglass5237@gmail.com>
2020-12-20 20:23:16 -0500

    Refactor to use bot commands instead of listening to the on_message event

--- robbot.py
+++ robbot.py
@@ -1,13 +1,15 @@
 import os
-import discord
 import json
 import random
 import re
+
+import discord
+from discord.ext import commands
+
 from dotenv import load_dotenv
 
 from requests.api import get
 from util import Bartender
-
 
 SHEBANGS = '.!$'
 CFG_FILENAME = 'config.json'
@@ -15,35 +17,25 @@ CFG_FILENAME = 'config.json'
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
-
 with open(CFG_FILENAME) as cfg:
     CFG = json.load(cfg)
 
 
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+bot = commands.Bot(command_prefix='.')
 
-@client.event
-async def on_message(message):
-    await check_react_ohwow(message)
+@bot.command(name='8ball')
+async def shake_8ball(ctx):
+    responses = CFG['8ball_responses']
+    rand_i = random.randint(0, len(responses) - 1)
+    response = responses[rand_i]
+    await ctx.send(response)
 
-    if not should_respond_msg(message):
-        return
+@bot.command(name='roll')
+async def roll(ctx, *args):
+    dice_pattern = r'[0-9]*(d)[0-9]+'  # optional number, d, at least one number, e.g. 2d8, d20, etc
 
-    cmd = get_msg_cmd(message)
-
-    if cmd == '8ball':
-        responses = CFG['8ball_responses']
-        rand_i = random.randint(0, len(responses) - 1)
-        response = responses[rand_i]
-        await message.channel.send(response)
-
-    if cmd == 'roll':
-        dice_pattern = r'[0-9]*(d)[0-9]+'  # optional number, d, at least one number, e.g. 2d8, d20, etc
-
-        match = re.search(dice_pattern, message.content)
+    for arg in args:
+        match = re.search(dice_pattern, arg)
         if match is not None:
             parts = match.group(0).split('d')
             try:
@@ -52,25 +44,54 @@ async def on_message(message):
                 num_rolls = 1
             max_roll = int(parts[1])
             response = f'Roll {num_rolls}d{max_roll}: '
-            for i in range(num_rolls):
+            for _ in range(num_rolls):
                 response += (str(random.randint(1, max_roll)) + ', ')
             response = response[:-2]
-            await message.channel.send(response)
-        else:
-            await message.channel.send(str(random.randint(1, 100)))
+            await ctx.send(response)
+            break  
+    else:
+        await ctx.send(str(random.randint(1, 100)))
+
+@bot.command(name='cocktail')
+async def get_cocktail(ctx):
+    try:
+        await ctx.send(embed=Bartender.get_random_drink().embed())
+    except Exception as ex:
+        print(ex)
+        await ctx.send('Ayo, your code is wack.')
+
+@bot.command()
+async def drink(ctx):
+    await get_cocktail(ctx)
+
+@bot.command()
+async def test(ctx):
+    await ctx.send('.test')
 
 
-    if cmd == 'bg':
-        await message.channel.send('hey')
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+
+@bot.event
+async def on_message(message):
+    await check_react_ohwow(message)
+
+    # if not should_respond_msg(message):
+    #     return
+
+    cmd = get_msg_cmd(message)
         
-    if cmd == 'drink' or cmd == 'cocktail':
-        await get_cocktail(message)
+    # if cmd == 'drink' or cmd == 'cocktail':
+    #     await get_cocktail(message)
+
+    await bot.process_commands(message)
     
     return
   
 
 def should_respond_msg(msg) -> bool:
-    if msg.author == client.user:  # Robbot is the author
+    if msg.author == bot.user:  # Robbot is the author
         return False
     if msg.content[0] not in SHEBANGS:
         return False
@@ -85,23 +106,10 @@ def get_msg_cmd(msg):
 
 async def check_react_ohwow(message):
     if 'oh wow' in message.content.lower():
-        for emoji in client.emojis:
+        for emoji in bot.emojis:
             if emoji.name == 'ohwow':
                 await message.add_reaction(emoji)
 
-'''
-@author: Keeth S.
-@dependencies: util/Bartender.py
-@desc: Returns a random drink embedded from the Drink object's  embed method
-@retunrs: async message back to channgel
-# TODO Optimize Drink Object.
-'''
-async def get_cocktail(msg):
-    try:
-        
-        await msg.channel.send(embed=Bartender.get_drink().embed())
-    except Exception as ex:
-        print(ex)
-        await msg.channel.send('Ayo, your code is wack.')
 
-client.run(TOKEN)
+
+bot.run(TOKEN)
