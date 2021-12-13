9ef474ebe0922242e6586a8e53a045129c158166
monkfishfillet <monkfishfillet@gmail.com>
2021-12-13 16:43:55 -0500

    Sloppy commit

--- robbot.py
+++ robbot.py
@@ -1,26 +1,11 @@
 import os
 import json
 import random
-<<<<<<< HEAD
-from discord.ext import commands
-from dotenv import load_dotenv
-=======
-import re
-import requests
 import asyncio
 import datetime
-
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
-
-from funcs.Bartender import Drink
-from funcs.Pin import Pin
-from util.Request import Request
-from util.Sanitizer import DrinkJsonSanitizer
-from util.Formatter import DrinkFormatter
-from util.Embedder import DrinkEmbedder, PinEmbedder
->>>>>>> develop
 from util import Rolls
 from util.Music import Music
 
@@ -91,19 +76,6 @@ async def roll(ctx, *args):
 
     await ctx.send(roll, tts=True)
 
-<<<<<<< HEAD
-@bot.event
-async def on_ready():
-    print(f'{bot.user} has connected to Discord!')
-
-@bot.event
-async def on_message(message):
-    await check_react_ohwow(message)
-
-    await bot.process_commands(message)
-    
-    return
-=======
 # Voice / Music
 @bot.command()
 async def join(ctx, *, channel: discord.VoiceChannel):
@@ -189,62 +161,15 @@ async def _download_msgs(ctx):
             json.dump(messages, f)
         await ctx.send('Done')
 
-
-
 @bot.command()
 async def test(ctx):
     await ctx.send('.test')
->>>>>>> develop
 
 async def check_react_ohwow(message):
     if 'oh wow' in message.content.lower():
         for emoji in bot.emojis:
             if emoji.name == 'ohwow':
                 await message.add_reaction(emoji)
-
-<<<<<<< HEAD
-=======
-
-'''
-@author: Keeth S.
-@desc: Returns a random drink embedded from the Drink object's  embed method
-@retunrs: async message back to channel
-'''
-@bot.command()
-async def drink(ctx):
-    try:
-        drink_json = request.get_drink_json()
-        drink = Drink(drink_json, DrinkJsonSanitizer, DrinkFormatter, DrinkEmbedder)
-        await ctx.send(embed = drink.embed)
-    except Exception as ex:
-        print(ex)
-        await ctx.send('Ayo, your code is wack.')
-
-@bot.command()
-async def cocktail(ctx):
-    await drink(ctx)
-
-'''
-@author: Keeth S.
-@desc: Sends a embed to the Pin channel when a user reply's to a message with .pin
-@retunrs: async message back to channel confirming message was pinned
-'''
-@bot.command()
-async def pin(ctx):
-    try:
-        if not ctx.message.reference:
-            await ctx.message.channel.send('You have to reply .pin to the message you want pinned.')
-            return
-        reply = await ctx.message.channel.fetch_message(ctx.message.reference.message_id)
-        pin = Pin(reply, PinEmbedder)
-        pin_channel = bot.get_channel(789771971532947486)
-        await pin_channel.send(embed=pin.embed)
-        await ctx.message.channel.send('You got it, bud.')
-    except Exception as ex:
-        print(ex)
-        await ctx.message.channel.send('Ayo, your code is wack.')
-
->>>>>>> develop
 
 load_cogs()
 bot.run(TOKEN)
